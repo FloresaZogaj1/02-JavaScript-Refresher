f77136b1229f050a238140175ce5b4159b672ee0
Floresa Zogaj <floresaa.zogaj@gmail.com>
2024-07-24 16:01:39 +0200

    02 Javascript Refresher

---
+++ public/assets/scripts/app.js
@@ -0,0 +1,157 @@
+ 
+ 
+// import {apiKey, abc as content} from "./util.js";
+// console.log(content);
+//console.log("Hello world");
+
+//let userMessage ="Hello ";
+//console.log(userMessage);
+
+
+//const userMessages="Js";
+
+//console.log(10 +5);
+//console.log("Hello"+ "Js");
+//console.log(10 ===5);
+
+//function greet(){
+  //  console.log("Hello");
+//}
+//greet();
+//greet();
+//greet();
+//greet();
+
+//function greet(UserName, message="Hello!"){
+  //  console.log(UserName);
+    //  console.log(message);
+
+//}
+//greet();
+// greetUser("Max", "Hello")
+
+//export default (UserName,message)=>{
+  //  console.log("Hello");
+   // return UserName+message;
+//};
+
+//const user={
+  //  name: "Max",
+   // age:30,
+    //greet(){
+      //  console.log("Hello");
+       // console/log(this.age)
+    //}
+//};
+//console.log(user.name);
+//user.greet();
+  
+
+//class User{
+//constructor(name,age){
+//this.name=name;
+//this.age=age;
+
+//}
+
+//greet(){
+//console.log("Hi");
+
+//}
+//}
+//const user1=new User("Manuel", 35);
+//console.log(user1);
+//user1.greet();
+
+
+// const hobbies=["sports", "cooking","reading"];
+// console.log(hobbies[0]);
+
+//     hobbies.push("Working");
+//     console.log(hobbies);
+//      const index =hobbies.findIndex((item)=>  item=== "sports");
+
+//    console.log(index);
+//    const editedHobbies=hobbies.map((item)=>({text:item}));
+//    console.log(editedHobbies);
+
+
+// const [firstName, lastName]=["Max", "Schwarzmuller"];
+
+// //const firstName=userNameData[0];
+// //const lastName=userNameData[1];
+
+// console.log(firstName);
+// console.log(lastName);
+
+
+
+// const {name:userName, age }={
+//          name:"Max",
+//          age:34
+
+// };
+// console.log(userName);
+// console.log(age);
+
+// const name=user.name;
+// const age=user.age;
+
+
+// const user={
+//     name:"Lil",
+//     age:30
+// };
+// const newHobbies=["Reading"];
+// const mergedHobbies=[...newHobbies, ...newHobbies];
+// console.log(mergedHobbies);
+
+// const extendedUser ={
+//     isAdmin:true,
+//     ...user
+// };
+// console.log(extendedUser);
+
+
+// const password =prompt ("Your password");
+// if(password==="Hello"){
+//     console.log("Hello works");
+// }else if(password==="hello"){
+//     console.log("hello works");
+// }else{
+//     console.log("Access not granted");
+// }
+// const hobbies=["sports", "Cooking"];
+// for (const hobby of hobbies){
+//     console.log(hobby);
+// }
+
+// const list=document.querySelector("ul");
+// list.remove();
+
+
+
+// function handleTimeout (){
+//     console.log("Timed out!");
+// }
+// const handleTimeout2 =()=>{
+//     console.log("Timed out.again!");
+// };
+// setTimeout(handleTimeout, 2000);
+// setTimeout(handleTimeout2, 3000);
+// setTimeout(() => {
+
+//     console.log('More timing out...');
+// }, 4000);
+
+// function init (){
+//     function greet(){
+//         console.log("Hi");
+//     }
+//     greet();
+// }
+// init();
+
+// const hobbies=["Sports","Cooking"];
+// hobbies.push("Working");
+// console.log(hobbies);
---
+++ public/assets/scripts/util.js
@@ -0,0 +1,3 @@
+ export default "Js1";
+ export let apiKey="Js";
+ export let abc ="abc";
